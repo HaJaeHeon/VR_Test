67137fcef503a9743a8cd97cb8643f0136755c03
HaJaeHeon <gkhen@naver.com>
2023-01-15 21:51:33 +0900

    haptic and reset

---
+++ VR_PlayerReset/Assets/Scripts/PlayerReset.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerReset : MonoBehaviour
+{
+   [SerializeField] private Transform resetTransform;
+   [SerializeField] private GameObject player;
+   [SerializeField] private Camera playerHead;
+    
+   [ContextMenu("Reset Position")]
+   public void ResetPosition()
+   {
+      var rotationAngleY = resetTransform.transform.rotation.eulerAngles.y 
+                           - playerHead.transform.rotation.eulerAngles.y;
+      player.transform.Rotate(0, rotationAngleY, 0);
+
+      var distanceDiff = resetTransform.position 
+                         - playerHead.transform.position;
+      player.transform.position += distanceDiff;
+   }
+}
